d4ebd333ec82a484e7a22257c3e20fc71eaa0d1a
phil <phil@rodgers.it>
2020-03-26 22:40:31 +0000

    Tidy and remove jQuery

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-
+var blockInput = false;
 
 function initStuff() {
+    blockInput = false;
     var operators = ['+', '-', '×', '÷'];
     var i = Math.floor(Math.random() * 4);
     var op1, op2;
@@ -24,14 +25,21 @@ function initStuff() {
 }
 
 function checkAnswer() {
+    if (blockInput) {
+        return;
+    }
     var el = document.getElementById("answer");
     el.onkeydown = function(e) {
-        var order = e.which;
-        console.log(order);
-        if (order === 8 || order === 46 || order === 109 || order === 173) {
+        if (blockInput) {
+            e.preventDefault();
+            return false;
+        }
+        var key = e.which;
+        console.log(key);
+        if (key === 8 || key === 46 || key === 109 || key === 173
+            || (key > 48 && key < 59) || (key > 95 && key < 106)) {
             return true;
-        } else if (order === 13) {
-            console.log("Enter");
+        } else if (key === 13) {
             var operator = document.getElementById("operator").innerText;
             var operand1 = parseInt(document.getElementById("operand1").innerText);
             var operand2 = parseInt(document.getElementById("operand2").innerText);
@@ -39,29 +47,27 @@ function checkAnswer() {
             var result;
             switch (operator) {
                 case "+":
-                    console.log("Plus");
                     result = operand1 + operand2;
                     break;
                 case "-":
-                    console.log("Minus");
                     result = operand1 - operand2;
                     break;
                 case "×":
-                    console.log("Multiply");
                     result = operand1 * operand2;
                     break;
                 case "÷":
-                    console.log("Divide");
                     result = operand1 / operand2;
                     break;
             }
-            console.log(answer, result);
             if (answer === result) {
+                blockInput = true;
                 document.getElementById("tick").style.display = 'block';
                 document.getElementById("cross").style.display = 'none';
                 var score = parseInt(document.getElementById("score").innerText);
                 document.getElementById("score").innerText = (score + 1) + "";
-                setTimeout(initStuff, 1000);
+                setTimeout(function() {
+                    initStuff();
+                    }, 1000);
             } else {
                 document.getElementById("cross").style.display = 'block';
                 document.getElementById("tick").style.display = 'none';
@@ -70,8 +76,6 @@ function checkAnswer() {
                     document.getElementById("cross").style.display = 'none';
                     }, 1000);
             }
-        } else if ((order > 48 && order < 59) || (order > 95 && order < 106)) {
-            console.log(order);
         } else {
             e.preventDefault();
             return false;
@@ -79,11 +83,5 @@ function checkAnswer() {
         return true;
     }
 }
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     initStuff();
-// }, false);
 
 
